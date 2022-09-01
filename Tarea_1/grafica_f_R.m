9c95f6c5e0c6493aecaa6ea9b229458761fa3b94
Alejandro <alehl1998@gmail.com>
2022-09-01 15:26:44 -0600

    Gráfica Lista

--- Tarea_1/grafica_f_R.m
+++ Tarea_1/grafica_f_R.m
@@ -1,4 +1,4 @@
-## Gráfica de f(R)
+%% Gráfica de f(R)
 
 R = linspace(0,3, 1001);
 f_R = 1.129241e-3 + 2.341033e-3*log(R) + 8.775468e-8*(log(R)).^3;
@@ -9,9 +9,10 @@ plot(R,f_R)
 grid on
 
 hold on
-plot(R,T_inv, 'x')
+plot(R,T_inv, 'g.')
 hold off
 
 title('Gráfica de f(R)')
 xlabel('Resistencia (ohms)')
 ylabel('Temperatura inversa (1/K)')
+
