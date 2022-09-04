aa164efecb3876d5b003d890dc45d473da7174d0
Carlos0130 <112665832+Carlos0130@users.noreply.github.com>
2022-09-04 16:37:19 -0600

    Funcion fzero

--- Tarea_1/Funcion fzero.m
+++ Tarea_1/Funcion fzero.m
@@ -1,9 +1,9 @@
 #FUNCIÓN FZERO#
 
 
-f = @(R)(1.129241*10^-3)+((2.341077*10^-3)*log(R))+((8.775468*10^-3)*(log(R))^3);
+f = @(R)(1.129241*10^-3)+(2.341077*10^-3)*log(R)+(8.775468*10^-8)*(log(R))^3-(1/291.1);
 vi=[2 3];
 
-[X, FVAL] = fzero(f,2)
+X = fzero(f,vi)
 
-fprintf("La aproximación de la raíz es:",X);
+fprintf("La aproximación de la raíz es: "); disp(X)
